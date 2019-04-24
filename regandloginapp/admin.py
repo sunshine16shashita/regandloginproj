b3535f6bbf7fceb11ca93a6a625a51afc02fc1bd
shashita <shashita16296@gmail.com>
2019-04-24 10:05:54 +0530

    In this project there is an application of django which shows how to register and login with admin.

--- regandloginapp/admin.py
+++ regandloginapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Reg
 # Register your models here.
+class Regadmin(admin.ModelAdmin):
+    list_display = ['user','fname','lname','dob','mobno']
+    list_filter = ['dob']
+    class meta:
+        model=Reg
+admin.site.register(Reg,Regadmin)
